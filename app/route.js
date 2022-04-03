dee050a73001bca35d4c73a75ad105d6d66a7423
aditi-growexxer <aditi.bamaniya@growexx.com>
2022-04-04 00:51:38 +0530

    emp commit

--- app/route.js
+++ app/route.js
@@ -1,13 +1,19 @@
 //Routing
 app.config(function ($routeProvider) {
     $routeProvider
-        .when('/login', {
+        .when('/home', {
             templateUrl: '/app/pages/login.html',
-            controller: 'authController'
+            controller: 'loginController'
         }).when('/dashboard', {
             templateUrl: '/app/pages/dashboard.html',
+        }).when('/employees_list', {
+            templateUrl: '/app/pages/employees_list.html',
+            controller: 'employeesController'
+        }).when('/create_new_employee', {
+            templateUrl: '/app/pages/create_new_employee.html',
+            controller: 'employeesController'
         }).otherwise({
-            redirectTo: '/login'
+            redirectTo: '/home'
         });
 });
 
